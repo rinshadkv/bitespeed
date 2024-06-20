b7eb9f412a9b8147ba66dd09b3a94ff3982f7d7a
rinshad <rinshadkv20@gmail.com>
2024-06-21 00:54:28 +0530

    load db credentails from env

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,10 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize("bitespeed", "postgres", "root", {
-  host: "localhost",
+dotenv.config(); 
+
+const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
+  host: process.env.DB_HOST!,
   dialect: "postgres",
 });
 
