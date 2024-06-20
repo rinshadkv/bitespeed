ee1b687c821cd8abe3b97e0553f56cf90e7246a3
rinshad <rinshadkv20@gmail.com>
2024-06-21 01:59:32 +0530

    chnaged env

--- dist/src/database.js
+++ dist/src/database.js
@@ -6,9 +6,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: "postgres",
-});
+const sequelize = new sequelize_1.Sequelize(process.env.DATABASE_URL);
 exports.default = sequelize;
--- src/database.ts
+++ src/database.ts
@@ -3,10 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config(); 
 
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
-  host: process.env.DB_HOST!,
-  port: Number(process.env.DB_PORT!),
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL!);
 
 export default sequelize;
